86afe3ba02dcf5f69ca107da78b4d5a9d422bf6a
Agrum <thomas.lgd@gmail.com>
2017-12-24 10:32:18 -0800

    Add boxed effects mono behaviours to attach to AoE abilities

---
+++ Assets/Scripts/Model/Ability/BoxedEffect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BoxedEffect : MonoBehaviour
+{
+	public bool IsSingleShot { get; internal set; }
+	public float ExpirationTime { get; internal set; }
+	private bool ranOnce = false;
+
+	public delegate void ObjectEnterDelegate(GameObject gameObject);
+	public delegate void ObjectLeaveDelegate(GameObject gameObject);
+
+	public event ObjectEnterDelegate ObjectEnterEvent;
+	public event ObjectLeaveDelegate ObjectLeaveEvent;
+
+	BoxedEffect()
+	{
+		IsSingleShot = true;
+	}
+
+	BoxedEffect(float expirationTime)
+	{
+		IsSingleShot = false;
+		ExpirationTime = expirationTime;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(enabled)
+			ObjectEnterEvent(other.gameObject);
+	}
+
+	void OnTriggerLeave(Collider other)
+	{
+		if (enabled)
+			ObjectLeaveEvent(other.gameObject);
+	}
+
+	void FixedUpdate()
+	{
+		if (IsSingleShot && ranOnce)
+			enabled = false;
+		if(!IsSingleShot && Time.time > ExpirationTime)
+			enabled = false;
+
+		ranOnce = true;
+	}
+}
