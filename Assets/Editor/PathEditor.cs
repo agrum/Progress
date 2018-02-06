40b830d6cd921a26ac1ac361deb74a76952b0282
Agrum <thomas.lgd@gmail.com>
2018-02-05 23:14:58 -0800

    2D path along xz plane capabilities for prologue to 2d collision.

---
+++ Assets/Editor/PathEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor {
+
+	PathCreator creator;
+	Path path;
+	Plane xz = new Plane(new Vector3(0f, 1f, 0f), 0f);
+
+	void OnSceneGUI()
+	{
+		Input();
+		Draw();
+	}
+
+	void Input()
+	{
+		Event guiEvent = Event.current;
+
+		if(guiEvent.type == EventType.mouseDown && guiEvent.button == 0 && guiEvent.shift)
+		{
+			float enter;
+			Ray worldRay = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition);
+			xz.Raycast(worldRay, out enter);
+			Vector3 mousePos = worldRay.GetPoint(enter);
+			Undo.RecordObject(creator, "Add path point");
+			path.AddSegment(new Vector2(mousePos.x, mousePos.z));
+		}
+	}
+
+	void OnEnable()
+	{
+		creator = (PathCreator)target;
+		if(creator.path == null)
+		{
+			creator.CreatePath();
+		}
+		path = creator.path;
+	}
+
+	void Draw()
+	{
+		Handles.color = Color.yellow;
+		for (int i = 0; i < path.NumSegments(); ++i)
+		{
+			Handles.DrawLine(new Vector3(path[i].x, 0, path[i].y), new Vector3(path[i+1].x, 0, path[i+1].y));
+		}
+
+		Handles.color = Color.red;
+		for(int i = 0; i < path.NumPoints(); ++i)
+		{
+			Vector3 newPosition = Handles.FreeMoveHandle(new Vector3(path[i].x, 0, path[i].y), Quaternion.identity, 0.1f, Vector3.zero, Handles.SphereHandleCap);
+
+			float enter;
+			Ray worldRay = new Ray(Camera.current.transform.position, newPosition - Camera.current.transform.position);
+			xz.Raycast(worldRay, out enter);
+			Vector3 newPositionOnPlane = worldRay.GetPoint(enter);
+			Vector2 newPosition2D = new Vector2(newPositionOnPlane.x, newPositionOnPlane.z);
+			if (path[i] != newPosition2D)
+			{
+				Undo.RecordObject(creator, "Move path point");
+				path[i] = newPosition2D;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Path/Path.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Path {
+
+	[SerializeField, HideInInspector]
+	List<Vector2> points = new List<Vector2>();
+
+	public Path(Vector2 center)
+	{
+		points.Add(center + Vector2.left);
+		points.Add(center + Vector2.right);
+	}
+
+	public Vector2 this[int i]
+	{
+		get
+		{
+			return points[i];
+		}
+		set
+		{
+			points[i] = value;
+		}
+	} 
+
+	public int NumPoints()
+	{
+		return points.Count;
+	}
+
+	public int NumSegments()
+	{
+		return points.Count - 1;
+	}
+
+	public void AddSegment(Vector2 anchorPoint)
+	{
+		points.Add(anchorPoint);
+	}
+
+	public Vector2[] GetPointsInSegment(int i)
+	{
+		return new Vector2[] { points[i], points[i+1] };
+	}
+}
