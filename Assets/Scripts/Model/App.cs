565d90f74addf342cb9e616d773592ea13fcffc7
Agrum <thomas.lgd@gmail.com>
2018-03-16 19:28:27 -0700

    add startup scene to wait for server answer on gameSettings request

--- Assets/Scripts/Model/App.cs
+++ Assets/Scripts/Model/App.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using SimpleJSON;
 using BestHTTP;
@@ -9,37 +10,36 @@ namespace West
 	public class App
 	{
 		static private JSONNode model = null;
+		static private HTTPRequest request = null;
 
 		static public JSONNode Model
 		{
 			get
 			{
-				if (model == null)
+				if (model == null && request == null)
 				{
 					//ask server for model base
-					HTTPRequest request = new HTTPRequest(
+					request = new HTTPRequest(
 						new System.Uri("http://127.0.0.1:3000/gameSettings/Classic"),
-						(HTTPRequest, HTTPResponse) => Debug.Log("gameSettings/Classic acquired"));
+						OnRequestFinished);
 					request.Send();
 
-					//wait for server response
-					for(int attempts = 0; attempts < 10 && request.State != HTTPRequestStates.Finished; ++attempts)
+					if(SceneManager.GetActiveScene().name != "Startup")
 					{
-						if (request.State == HTTPRequestStates.Aborted ||
-							request.State == HTTPRequestStates.ConnectionTimedOut ||
-							request.State == HTTPRequestStates.Error ||
-							request.State == HTTPRequestStates.TimedOut)
-						{
-							Debug.Log(request.State);
-							Application.Quit();
-						}
-						System.Threading.Thread.Sleep(100);
+						SceneManager.LoadScene("Startup");
 					}
-					model = JSON.Parse(request.Response.DataAsText);
 				}
 				return model;
 			}
 		}
 
+		static private void OnRequestFinished(HTTPRequest _request, HTTPResponse response)
+		{
+			if (request.State == HTTPRequestStates.Finished)
+			{
+				model = JSON.Parse(request.Response.DataAsText);
+			}
+			request = null;
+		}
 	}
 }
---
+++ Assets/Scripts/Startup/StartupManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace West
+{
+	public class StartupManager : MonoBehaviour
+	{
+		public Text text;
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if(App.Model != null)
+			{
+				SceneManager.LoadScene("World");
+			}
+			else if(text != null)
+			{
+				text.text = "Starting up";
+			}
+		}
+	}
+}
