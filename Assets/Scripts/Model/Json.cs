e1492ac766190b90fb4e3a6158bf8ced3dc47e12
Agrum <thomas.lgd@gmail.com>
2018-06-12 21:15:11 -0700

    Json Model object

---
+++ Assets/Scripts/Model/Json.cs
@@ -0,0 +1,33 @@
+﻿using SimpleJSON;
+
+namespace West.Model
+{
+	public class Json
+	{
+		public delegate void OnChanged(string key);
+		public event OnChanged ChangedEvent = delegate { };
+
+		private JSONObject json = new JSONObject();
+
+		~Json()
+		{
+			ChangedEvent = null;
+		}
+
+		public JSONNode this[string key]
+		{
+			get
+			{
+				return json[key];
+			}
+			set
+			{
+				if (json[key] != value)
+				{
+					json[key] = value;
+					ChangedEvent(key);
+				}
+			}
+		}
+	}
+}
