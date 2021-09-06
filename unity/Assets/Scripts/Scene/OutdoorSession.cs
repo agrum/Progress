16feb08892a74998e77c4eab4fb660603686a6b0
thomas.lgd <thomas.lgd@gmail.com>
2021-09-05 19:10:13 -0700

    experiement with unity's tilemap

--- unity/Assets/Scripts/Scene/OutdoorSession.cs
+++ unity/Assets/Scripts/Scene/OutdoorSession.cs
@@ -3,16 +3,39 @@ using SimpleJSON;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.Scene
 {
     public class OutdoorSession : MonoBehaviour
     {
+        public class Tile : TileBase
+        {
+            public GameObject prefab;
+
+            public Tile(GameObject prefab_)
+            {
+                prefab = prefab_;
+            }
+
+            public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+            {
+                // Read docs for an explanation on this.
+                tilemap.RefreshTile(position);
+            }
+
+            public override void GetTileData(Vector3Int position, ITilemap tileMap, ref UnityEngine.Tilemaps.TileData tileData)
+            {
+                tileData.gameObject = prefab;
+            }
+        }
+
         public Context.PlayerNew player;
         public string layoutId;
         public int size;
-        public Context.Terrain.Tile[,] tiles;
+        public GameObject DummyTile;
+        public Tilemap Tilemap;
 
         // Use this for initialization
         IEnumerator Start()
@@ -20,13 +43,15 @@ namespace Assets.Scripts.Scene
             yield return StartCoroutine(App.Content.Layouts.Load());
 
             var layout = App.Content.Layouts.OutdoorLayouts[layoutId];
-            tiles = new Context.Terrain.Tile[size, size];
 
-            for (int col = 0; col < tiles.GetLength(0); col++)
+            Tile tile = ScriptableObject.CreateInstance<Tile>();
+            tile.prefab = DummyTile;
+
+            for (int col = 0; col < size; col++)
             {
-                for (int row = 0; row < tiles.GetLength(1); row++)
+                for (int row = 0; row < size; row++)
                 {
-                    tiles[col, row] = new Context.Terrain.Tile();
+                    Tilemap.SetTile(new Vector3Int(col, row, 0), tile);
                 }
             }
         }
